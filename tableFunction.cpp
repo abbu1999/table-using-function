57cf7767a59456cb050f0668b0ad98df79ea7361
abbu1999 <abhishekbisht559@gmail.cm>
2020-05-25 23:38:16 +0530

    inital commit

---
+++ tableFunction.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void table()
+{
+		int n;
+	cout<<"Enter the number=";
+	cin>>n;
+	for(int i=1;i<=10;i++)
+	{
+		cout<<n*i<<endl;
+		
+	}
+}
+int main()
+{
+	table();
+	table();
+	return 0;
+}
